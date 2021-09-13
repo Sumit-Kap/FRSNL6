9b5442423fbe314d2e7b9d7927e8da8291c8a02d
Sumit Kapoor <sumwin@Sumits-MacBook-Air.local>
2021-09-13 16:02:53 +0530

    Add lecture 4 & 5 code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,50 @@
 import React from "react";
-
-export default class Home extends React.Component {
+interface Props {}
+interface ResponseData {
+  id: number;
+  price: number;
+  description: string;
+  image: string;
+}
+interface State {
+  response: ResponseData[];
+}
+export default class Home extends React.PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      response: [],
+    };
+  }
+  getProductsData = async () => {
+    const apiResponse = await fetch("https://fakestoreapi.com/products/");
+    const responseData = await apiResponse.json();
+    this.setState({
+      response: responseData,
+    });
+    console.log("print data", responseData);
+  };
+  componentDidMount() {
+    this.getProductsData();
+  }
   render() {
-    return <h1>Hi Geeks this is my homepage</h1>;
+    const { response } = this.state;
+
+    if (response.length === 0) {
+      return <div className="loader">Loader........</div>;
+    }
+    return (
+      <div>
+        <div className="product-list">
+          {response.map((resp) => (
+            <div className="product-cards">
+              <img src={resp.image} />
+              <div className="product-description">{resp.description}</div>
+              <div className="product-price">{resp.price}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
